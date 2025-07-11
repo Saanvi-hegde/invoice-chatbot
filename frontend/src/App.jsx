c1ee6692389e7d242a94c578f88f62120140a755
Saanvi-hegde <saanvi.ai22@bmsce.ac.in>
2025-07-11 20:04:07 +0530

    Updated App.jsx with new features

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,18 @@
- 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Compare from "./pages/Compare"; // (create this later)
+import History from "./pages/History"; // (create this later)
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/compare" element={<Compare />} />
+        <Route path="/history" element={<History />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
